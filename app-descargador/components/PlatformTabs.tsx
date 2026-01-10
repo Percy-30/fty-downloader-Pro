40e17fef92c65f5572a05f6e392bcd18983db04a
Percy <achataipepercy2000@gmail.com>
2026-01-10 17:40:38 -0500

    UI: Update TikTok icon

--- app-descargador/components/PlatformTabs.tsx
+++ app-descargador/components/PlatformTabs.tsx
@@ -33,7 +33,7 @@ const platforms = [
     name: 'TikTok',
     icon: (
       <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-        <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z" />
+        <path d="M19.589 6.686a4.793 4.793 0 0 1-3.77-4.245V2h-3.445v13.672a2.896 2.896 0 0 1-5.201 1.743l-.002-.001.002.001a2.895 2.895 0 0 1 3.183-4.51v-3.5a6.329 6.329 0 0 0-5.394 10.692 6.33 6.33 0 0 0 10.857-4.424V8.687a8.182 8.182 0 0 0 4.773 1.526V6.79a4.831 4.831 0 0 1-1.003-.104z" />
       </svg>
     ),
     color: 'bg-black'
@@ -49,8 +49,8 @@ export default function PlatformTabs({ activePlatform, onPlatformChange }: Platf
             key={platform.id}
             onClick={() => onPlatformChange(platform.id)}
             className={`flex items-center space-x-2 px-6 py-3 rounded-lg font-semibold transition-all duration-200 justify-center w-full sm:w-auto sm:flex-1 ${activePlatform === platform.id
-                ? `${platform.color} text-white shadow-md`
-                : 'text-gray-600 hover:bg-gray-100'
+              ? `${platform.color} text-white shadow-md`
+              : 'text-gray-600 hover:bg-gray-100'
               }`}
           >
             {platform.icon}
